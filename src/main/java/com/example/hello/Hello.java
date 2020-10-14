9e1794658897a6d45a7c8f48928babed2c339b98
Jacob Blazusiak <jblazusi@ualberta.ca>
2020-10-13 22:36:18 -0600

    Initialization

---
+++ src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.example.hello;
+
+public class Hello {
+	
+	public static void main(String [] args) {
+		//String s = null;
+		//s.length();
+		System.out.println("Hello, world!");
+	}
+	
+}
